edf06b3017849ab3c7a56e06437cc7f85e7ef3ec
dariyatum <dariyatum412@gmail.com>
2025-11-28 08:54:57 +0700

    jenh

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ fetch("/public/data/properties.json")
   .then((response) => response.json())
   .then((data) => {
     if (data.length > 0) {
-      for (let i = 0; i < 2; i++) {
+      for (let i = 0; i < 4; i++) {
         sectionPropertiesElement.innerHTML += `
         <a class="card" href="/pages/property/${data[i].title.en}">
             <img class="card__media" src="${data[i].images}"
