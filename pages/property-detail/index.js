c6a732adb9b7603e5e5f89ae531908479222e53b
DaraCOOKED <dara.somnag020@gmail.com>
2025-12-01 09:47:11 +0700

    detail

--- pages/property-detail/index.js
+++ pages/property-detail/index.js
@@ -6,7 +6,7 @@ fetch("../../public/data/properties.json")
     if (data.length > 0) {
       console.log(data);
       
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < 4; i++) {
         sectionPropertiesElement.innerHTML += `
         <a class="card" href="/pages/property/${data[i].title.en}">
             <img class="images-photo" src="${data[i].images}"
