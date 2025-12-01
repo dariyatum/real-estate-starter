ebf141dc8eb564446eff065a1377bf0ba299f254
chheunnita169-dot <chheunnita169@gmail.com>
2025-12-01 16:28:48 +0700

    ok

--- pages/Listing Page/index.js
+++ pages/Listing Page/index.js
@@ -8,16 +8,20 @@ fetch("../../public/data/properties.json")
       
       for (let i = 0; i < data.length; i++) {
         sectionPropertiesElement.innerHTML += `
-        <a class="card" href="/pages/property/${data[i].title.en}">
+               <a class="card" href="/pages/property/${data[i].title.en}">
             <img class="card__media" src="${data[i].images}"
                 alt="${data[i].title.en}" />
             <div class="card__body">
                 <h3 class="card__title">${data[i].title.en}</h3>
-                <div class="card__meta">${data[i].price} ${data[i].currency} / month</div>
-
-                <span class="badge">Available</span>
+                <div class="card__location"> <i class="fa-solid fa-location-dot" style="color: #2ec27e;"></i>${data[i].location.city}  </div>
+                <div class="card__tion">${data[i].location.district}  </div>
+                <div class="card__meta"> ${data[i].price} ${data[i].currency} </div>
+                <div class="card__icon"> <i class="fa-solid fa-bed" style="color: #2ec27e;"></i>${data[i].bedrooms} </div>
+                <div class="card__icon"> <i class="fa-solid fa-bath" style="color: #2ec27e;"></i>${data[i].bathrooms} </div>
+                <span class="badge">View</span>
             </div>
         </a>
+        
       `;
       }
     }
--- pages/buy/index.js
+++ pages/buy/index.js
@@ -13,11 +13,11 @@ fetch("../../public/data/properties.json")
                 alt="${data[i].title.en}" />
             <div class="card__body">
                 <h3 class="card__title">${data[i].title.en}</h3>
-                <div class="card__location">${data[i].location.city}  </div>
+                <div class="card__location"> <i class="fa-solid fa-location-dot" style="color: #2ec27e;"></i>${data[i].location.city}  </div>
                 <div class="card__tion">${data[i].location.district}  </div>
-                <div class="card__meta">${data[i].price} ${data[i].currency} </div>
-                <div class="card__icon">${data[i].bedrooms} </div>
-                <div class="card">${data[i].bathrooms} </div>
+                <div class="card__meta"> ${data[i].price} ${data[i].currency} </div>
+                <div class="card__icon"> <i class="fa-solid fa-bed" style="color: #2ec27e;"></i>${data[i].bedrooms} </div>
+                <div class="card__icon"> <i class="fa-solid fa-bath" style="color: #2ec27e;"></i>${data[i].bathrooms} </div>
                 <span class="badge">View</span>
             </div>
         </a>
